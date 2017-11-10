63e574d211987b7b0d26609ec3fb23a8a87ca18c
Bryce Chen <bryce1_chen@peatroncorp.com>
2017-11-10 14:00:53 +0800

    Update the style of links

---
+++ js/links.js
@@ -0,0 +1,13 @@
+/* JavaScript practice from coursera courses */
+
+/* Journals */
+function myJournalLinks1() {
+	window.open("http://ieeexplore.ieee.org/document/7152725/");
+}
+
+/* Conferences */
+function myConferenceLinks1() {
+	window.open("http://ieeexplore.ieee.org/document/6932347/");
+}
+
+/* Patents */
