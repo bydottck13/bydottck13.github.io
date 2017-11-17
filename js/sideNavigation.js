ff00b061d4d2c482545c8249a60d0f86eaa47802
Bryce Chen <bryce1_chen@peatroncorp.com>
2017-11-17 17:06:06 +0800

    Add a new page for protfolio

--- js/sideNavigation.js
+++ js/sideNavigation.js
@@ -4,6 +4,7 @@ function openNav() {
 	document.getElementById("home").style.left = "0";
 	document.getElementById("about").style.left = "0";
 	document.getElementById("publication").style.left = "0";
+	document.getElementById("portfolio").style.left = "0";
 	document.getElementById("personal").style.left = "0";
 	document.getElementById("menu").style.display = "none";
 	document.getElementById("close").style.display = "inline";
@@ -14,6 +15,7 @@ function closeNav() {
 	document.getElementById("home").removeAttribute("style");
 	document.getElementById("about").removeAttribute("style");
 	document.getElementById("publication").removeAttribute("style");
+	document.getElementById("portfolio").removeAttribute("style");
 	document.getElementById("personal").removeAttribute("style");
 	document.getElementById("menu").style.display = "inline";
 	document.getElementById("close").style.display = "none";
